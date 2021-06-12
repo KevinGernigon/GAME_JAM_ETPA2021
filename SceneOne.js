8e72e017763995191caee350f3f7b54d956379e9
KevinGernigon <k.gernigon@rns.ecolescreatives.com>
2021-06-12 16:06:30 +0200

    proto_v2

--- SceneOne.js
+++ SceneOne.js
@@ -10,6 +10,12 @@ var texte_terreau;
 var texte_oxygene;
 var texte_population;
 
+//demandes//
+var demande_eau;
+var demande_soleil;
+var demande_oxygene;
+var demande_terreau;
+
 //plantes//
 var plante_carnivore;
 var plante_astronaute;
@@ -18,6 +24,12 @@ var plante_scientifique;
 var plante_banquiere;
 var plante_exploratrice;
 var plante_coffre;
+var new_plante_astronaute;
+var new_plante_meteo;
+var new_plante_scientifique;
+var new_plante_banquiere;
+var new_plante_exploratrice;
+var new_plante_coffre;
 
 //generation aléa et booleans pour check//
 var plante_presente = false;
@@ -35,6 +47,25 @@ var picto_soleil;
 var picto_oxygene;
 var picto_terreau;
 var picto_population;
+
+//rng coffre//
+var numero_coffre;
+var coffre_genere = false;
+
+//proc banquier//
+var proc_banquier = false;
+var proc_200 = false;
+var proc_400 = false;
+var proc_600 = false;
+var banquier_doit_partir = false;
+
+//temporalite//
+var jour = 0;
+var texte_jour;
+var compteur_rencontre = 0;
+
+//objectif espace//
+var objectif = 0;
 
 class SceneOne extends Phaser.Scene{
     constructor(){
@@ -78,21 +109,19 @@ class SceneOne extends Phaser.Scene{
         });
         
         //ressources hud//
-        picto_eau = this.add.sprite(50, 390, 'picto_eau');
-        texte_eau = this.add.text(70, 382, eau, {fontSize: '20px', fill: '#FFF'});
-        
+        picto_eau = this.add.sprite(50, 390, 'picto_eau'); 
         picto_soleil = this.add.sprite(120, 390, 'picto_soleil');
-        texte_soleil = this.add.text(140, 382, soleil, {fontSize: '20px', fill: '#FFF'});
-        
         picto_terreau = this.add.sprite(190, 390, 'picto_terreau');
-        texte_terreau = this.add.text(210, 382, terreau, {fontSize: '20px', fill: '#FFF'});
-        
         picto_oxygene = this.add.sprite(260, 390, 'picto_oxygene');
-        texte_oxygene = this.add.text(280, 382, oxygene, {fontSize: '20px', fill: '#FFF'});
-        
         picto_population = this.add.sprite(330, 390, 'picto_population');
-        texte_population =this.add.text(350, 382, population, {fontSize: '20px', fill: '#FFF'});
+
         
+        //pictos ressources demandees//
+        demande_eau = this.add.sprite(650, 120, 'picto_eau').setVisible(false);
+        demande_soleil = this.add.sprite(650, 180, 'picto_soleil').setVisible(false);
+        demande_oxygene = this.add.sprite(650, 240, 'picto_oxygene').setVisible(false);
+        demande_terreau = this.add.sprite(650, 300, 'picto_terreau').setVisible(false);
+              
         //assets//
         plante_carnivore = this.add.sprite(100, 200, 'plante_carnivore');
         
@@ -112,116 +141,244 @@ class SceneOne extends Phaser.Scene{
         bouton_oui.on('pointerdown', function(){
             bouton_oui.setVisible(false);
             bouton_non.setVisible(false);
-            if (plante_presente == true && numero_random == 0 && plante_cree == true){
-                terreau = terreau + 50;
-                oxygene = oxygene - 50;
+            if (plante_presente == true && numero_random == 0 && plante_cree == true && proc_banquier == false){
+                eau = eau - 1;
+                soleil = soleil - 1;
+                oxygene = oxygene + 1;
+                terreau = terreau +1;
+                new_plante_exploratrice.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_exploratrice();       
             }
-            if (plante_presente == true && numero_random == 1 && plante_cree == true){
-                oxygene = oxygene -100;
+            if (plante_presente == true && numero_random == 1 && plante_cree == true && proc_banquier == false){
+                oxygene = oxygene - 1;
+                objectif = objectif + 1;
+                new_plante_astronaute.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_astronaute();
             }
-            if (plante_presente == true && numero_random == 2 && plante_cree == true){
-                terreau = terreau + 50;
-                oxygene = oxygene - 50;
+            if (plante_presente == true && numero_random == 2 && plante_cree == true && proc_banquier == false){
+                if (numero_coffre == 0){
+                    eau = eau + 1;
+                }
+                if (numero_coffre == 1){
+                    eau = eau - 1;
+                }
+                if (numero_coffre == 2){
+                    soleil = soleil + 1;
+                }
+                if (numero_coffre == 3){
+                    soleil = soleil - 1;
+                }
+                if (numero_coffre == 4){
+                    oxygene = oxygene + 1;
+                }
+                if (numero_coffre == 5){
+                    oxygene = oxygene - 1;
+                }
+                if (numero_coffre == 6){
+                    terreau = terreau + 1;
+                }
+                if (numero_coffre == 7){
+                    terreau = terreau - 1;
+                }
+                if (numero_coffre == 8){
+                    population = population + 1;
+                }
+                if (numero_coffre == 9){
+                    population = population - 1;
+                }
+                new_plante_coffre.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_coffre();
             }
-            if (plante_presente == true && numero_random == 3 && plante_cree == true){
-                terreau = terreau + 50;
-                oxygene = oxygene - 50;
+            if (plante_presente == true && numero_random == 3 && plante_cree == true && proc_banquier == false){
+                eau = eau + 1;
+                soleil = soleil + 1;
+                oxygene = oxygene - 2;
+                new_plante_meteo.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_meteo();
             }
-            if (plante_presente == true && numero_random == 4 && plante_cree == true){
-                terreau = terreau + 50;
-                oxygene = oxygene - 50;
+            if (plante_presente == true && numero_random == 4 && plante_cree == true && proc_banquier == false){
+                terreau = terreau - 1;
+                objectif = objectif + 1;
+                new_plante_scientifique.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_scientifique();
             }
-            if (plante_presente == true && numero_random == 5 && plante_cree == true){
-                terreau = terreau + 50;
-                oxygene = oxygene - 50;
+            if(banquier_doit_partir == true){
+                new_plante_banquiere.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_banquier();
             }
+
         }, this);
         
         bouton_non.on('pointerdown', function(){
             bouton_oui.setVisible(false);
             bouton_non.setVisible(false);
+            if (plante_presente == true && numero_random == 0 && plante_cree == true && proc_banquier == false){
+                new_plante_exploratrice.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_exploratrice();
+            }
+            if (plante_presente == true && numero_random == 1 && plante_cree == true && proc_banquier == false){
+                new_plante_astronaute.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_astronaute();
+            }
+            if (plante_presente == true && numero_random == 2 && plante_cree == true && proc_banquier == false){
+                new_plante_coffre.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_coffre();
+            }
+            if (plante_presente == true && numero_random == 3 && plante_cree == true && proc_banquier == false){
+                new_plante_meteo.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_meteo();
+            }
+            if (plante_presente == true && numero_random == 4 && plante_cree == true && proc_banquier == false){
+                new_plante_scientifique.setVelocityX(300);
+                demande_eau.setVisible(false);
+                demande_soleil.setVisible(false);
+                demande_terreau.setVisible(false);
+                demande_oxygene.setVisible(false);
+                remove_scientifique();
+            }
         }, this);
         
         
     }
     update(){
         
-        //refresh texte ressources//
-        texte_eau.setText(eau);
-        texte_soleil.setText(soleil);
-        texte_terreau.setText(terreau);
-        texte_oxygene.setText(oxygene);
-        texte_population.setText(population);
+        //objectif atteint//
+        if (objectif == 10){
+            plante_presente = true;
+            plante_cree = true;
+            banquier_doit_partir = true;
+            this.add.texte(400, 200, 'youpi direction espace', {fontSize: '48px', fill: '#FFF'});
+        }
+        
+        if(plante_presente == true && plante_cree == true && numero_random == 2 && coffre_genere == false && proc_banquier == false){
+            coffre_genere = true;
+            numero_coffre = getRandomInt(9);
+        }
+        
         
         //generation aléatoire//
         if (plante_presente == false && keys.space.isDown){
             plante_presente = true;
-            numero_random = getRandomInt(6);
+            numero_random = getRandomInt(5);
+            compteur_rencontre = compteur_rencontre + 1;
         }
         
-        if (plante_presente == true && numero_random == 0 && plante_cree == false){
+        //arrivee plantes a l'ecran
+        if (plante_presente == true && numero_random == 0 && plante_cree == false && proc_banquier == false && compteur_rencontre < 4){
             plante_cree = true;
-            plante_exploratrice.create(1400, 200, 'plante_exploratrice');
-            plante_exploratrice.setVelocityX(-300);
+            new_plante_exploratrice = plante_exploratrice.create(1400, 200, 'plante_exploratrice');
+            new_plante_exploratrice.setVelocityX(-300);
             setTimeout(function(){
-                plante_exploratrice.setVelocityX(0);
+                new_plante_exploratrice.setVelocityX(0);
+                bouton_oui.setVisible(true);
+                bouton_non.setVisible(true);
+                demande_eau.setVisible(true);
+                demande_soleil.setVisible(true);
+                demande_oxygene.setVisible(true);
+                demande_terreau.setVisible(true);
+            }, 2000);
+        }
+        
+        if (plante_presente == true && numero_random == 1 && plante_cree == false && proc_banquier == false && compteur_rencontre < 4){
+            plante_cree = true;
+            new_plante_astronaute = plante_astronaute.create(1400, 200, 'plante_astronaute');
+            new_plante_astronaute.setVelocityX(-300);
+            setTimeout(function(){
+                new_plante_astronaute.setVelocityX(0);
+                bouton_oui.setVisible(true);
+                bouton_non.setVisible(true);
+                demande_oxygene.setVisible(true);
+            }, 2000);
+        }
+        
+        if (plante_presente == true && numero_random == 2 && plante_cree == false && proc_banquier == false && compteur_rencontre < 4){
+            plante_cree = true;
+            new_plante_coffre = plante_coffre.create(1400, 200, 'plante_coffre');
+            new_plante_coffre.setVelocityX(-300);
+            setTimeout(function(){
+                new_plante_coffre.setVelocityX(0);
                 bouton_oui.setVisible(true);
                 bouton_non.setVisible(true);
             }, 2000);
         }
         
-        if (plante_presente == true && numero_random == 1 && plante_cree == false){
+        if (plante_presente == true && numero_random == 3 && plante_cree == false && proc_banquier == false && compteur_rencontre < 4){
             plante_cree = true;
-            plante_astronaute.create(1400, 200, 'plante_astronaute');
-            plante_astronaute.setVelocityX(-300);
+            new_plante_meteo = plante_meteo.create(1400, 200, 'plante_meteo');
+            new_plante_meteo.setVelocityX(-300);
             setTimeout(function(){
-                plante_astronaute.setVelocityX(0);
+                new_plante_meteo.setVelocityX(0);
                 bouton_oui.setVisible(true);
                 bouton_non.setVisible(true);
+                demande_eau.setVisible(true);
+                demande_soleil.setVisible(true);
+                demande_oxygene.setVisible(true);
             }, 2000);
         }
         
-        if (plante_presente == true && numero_random == 2 && plante_cree == false){
+        if (plante_presente == true && numero_random == 4 && plante_cree == false && proc_banquier == false && compteur_rencontre < 4){
             plante_cree = true;
-            plante_banquiere.create(1400, 200, 'plante_banquiere');
-            plante_banquiere.setVelocityX(-300);
+            new_plante_scientifique = plante_scientifique.create(1400, 200, 'plante_scientifique');
+            new_plante_scientifique.setVelocityX(-300);
             setTimeout(function(){
-                plante_banquiere.setVelocityX(0);
+                new_plante_scientifique.setVelocityX(0);
                 bouton_oui.setVisible(true);
                 bouton_non.setVisible(true);
+                demande_terreau.setVisible(true);
             }, 2000);
         }
         
-        if (plante_presente == true && numero_random == 3 && plante_cree == false){
+        //reset journee//
+        if(compteur_rencontre == 4 && banquier_doit_partir == false){
+            proc_banquier = true;
+            plante_presente = true;
             plante_cree = true;
-            plante_coffre.create(1400, 200, 'plante_coffre');
-            plante_coffre.setVelocityX(-300);
+            banquier_doit_partir = true;
+            new_plante_banquiere = plante_banquiere.create(1400, 200, 'plante_banquiere');
+            new_plante_banquiere.setVelocityX(-300);
             setTimeout(function(){
-                plante_coffre.setVelocityX(0);
+                new_plante_banquiere.setVelocityX(0);
                 bouton_oui.setVisible(true);
-                bouton_non.setVisible(true);
-            }, 2000);
-        }
-        
-        if (plante_presente == true && numero_random == 4 && plante_cree == false){
-            plante_cree = true;
-            plante_meteo.create(1400, 200, 'plante_meteo');
-            plante_meteo.setVelocityX(-300);
-            setTimeout(function(){
-                plante_meteo.setVelocityX(0);
-                bouton_oui.setVisible(true);
-                bouton_non.setVisible(true);
-            }, 2000);
-        }
-        
-        if (plante_presente == true && numero_random == 5 && plante_cree == false){
-            plante_cree = true;
-            plante_scientifique.create(1400, 200, 'plante_scientifique');
-            plante_scientifique.setVelocityX(-300);
-            setTimeout(function(){
-                plante_scientifique.setVelocityX(0);
-                bouton_oui.setVisible(true);
-                bouton_non.setVisible(true);
             }, 2000);
         }
     }
@@ -229,4 +386,61 @@ class SceneOne extends Phaser.Scene{
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function remove_exploratrice(){
+    setTimeout(function(){
+        new_plante_exploratrice.destroy();
+        plante_presente = false;
+        plante_cree = false;
+    }, 2000);
+}
+function remove_astronaute(){
+    setTimeout(function(){
+        new_plante_astronaute.destroy();
+        plante_presente = false;
+        plante_cree = false;
+    }, 2000);
+}
+function remove_coffre(){
+    setTimeout(function(){
+        new_plante_coffre.destroy();
+        plante_presente = false;
+        plante_cree = false;
+    }, 2000);
+}
+function remove_meteo(){
+    setTimeout(function(){
+        new_plante_meteo.destroy();
+        plante_presente = false;
+        plante_cree = false;
+    }, 2000);
+}
+function remove_scientifique(){
+    setTimeout(function(){
+        new_plante_scientifique.destroy();
+        plante_presente = false;
+        plante_cree = false;
+    }, 2000);
+}
+function remove_banquier(){
+    setTimeout(function(){
+        new_plante_banquiere.destroy();
+        banquier_doit_partir = false;
+        proc_banquier = false;
+        plante_presente = false;
+        plante_cree = false;
+            
+    }, 2000);
+    compteur_rencontre = 0;  
+    jour = jour + 1;
+    if (eau + soleil >= 2 && eau + soleil < 4){
+        oxygene = oxygene + 2;
+    }
+    if (eau + soleil >= 4 && eau + soleil < 6){
+        oxygene = oxygene + 4;
+    }
+    if (eau + soleil >= 6){
+        oxygene = oxygene + 6;
+    }
 }
